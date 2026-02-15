4ba514b08a1d8a022316a2d57ae4725a74fea3ba
Veronika-Bortkova <shishiga79@gmail.com>
2026-02-16 01:28:23 +0200

    button style

--- main.js
+++ main.js
@@ -3,6 +3,16 @@ let selectNameValue = document.getElementById("myList");
 let inputNameValuePair = document.getElementById("NameValuePair");
 let buttonAdd = document.getElementById("buttonAdd");
 let DivInCorrectFormat = document.getElementById("inCorrectFormat");
+let btns = document.getElementsByTagName("button");
+for (const btn of btns) {
+    btn.addEventListener("touchstart", () => {
+        btn.style.transform = "scale(0.8)";   // уменьшаем кнопку до 80%
+    });
+    btn.addEventListener("touchend", () => {
+        btn.style.transform = "scale(1)";     // возвращаем обратно
+    });
+}
+
 form.addEventListener("submit",function (ev){
     ev.preventDefault();
 
@@ -14,15 +24,15 @@ form.addEventListener("submit",function (ev){
             }
         }
             if (arrNumberEqualSigns.length!=1){
-                DivInCorrectFormat.innerText = "Enter the information in the correct format";// если в строке нет знака "="  или их больше одного, выдаю сообщение об ошибке
+                DivInCorrectFormat.innerText = "Enter the information in the correct format";// если в строке нет знака "=" или их больше одного, выдаю сообщение об ошибке
             } else {
                 let Name_Value = value.split("=");
-                if (!/^[\p{L}\p{N}][\p{L}\p{N}]*\s*$/u.test(Name_Value[0]) || !/^\s*[\p{L}\p{N}]+$/u.test(Name_Value[1])){//делаю проверку строк до знака равно и после. До знака равно строка долдна начинаться только с численно-буквенных символов, далее содержать любое (в том числе ноль) кол-во буквенно-числовых символов, конец строки - любое кол-во пробелов. После знака равно строка должна начинаться с любого кол-ва пробелов (в том числе ноль - ни одного пробела), далее любое кол-во буквенно-численных символов, но не менее одного символа. Юникод фоаг для того чтобы работало для букв и чисел любого алфавита.
+                if (!/^[\p{L}\p{N}][\p{L}\p{N}]*\s*$/u.test(Name_Value[0]) || !/^\s*[\p{L}\p{N}]+$/u.test(Name_Value[1])){//делаю проверку строк до знака равно и после. Строка находящаяся до знака "=" должна начинаться только с численно-буквенных символов, далее содержать любое (в том числе ноль) кол-во буквенно-числовых символов, конец строки - любое кол-во пробелов. После знака "=" строка должна начинаться с любого кол-ва пробелов (в том числе ноль - ни одного пробела), далее любое кол-во буквенно-численных символов, но не менее одного символа. Юникод флаг для того чтобы работало для букв и чисел любого алфавита.
                     DivInCorrectFormat.innerText = "Enter the information in the correct format";
                  } else{
-                    DivInCorrectFormat.innerText ="";
+                    DivInCorrectFormat.innerText ="";//убираю сообщение об ошибке
                     let optionPair = document.createElement("option");
-                    optionPair.innerText =(Name_Value[0]+"="+Name_Value[1]).replaceAll(" ","");
+                    optionPair.innerText =(Name_Value[0]+"="+Name_Value[1]).replaceAll(" ","");//формирую строку, которую кладем в список, убираю пробелы перед и до знака "="
                     selectNameValue.append(optionPair);
                      }
             }
