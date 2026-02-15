fb3bfa73c554b2d03cd6b4273e1d4f13c526336c
Veronika-Bortkova <shishiga79@gmail.com>
2026-02-16 01:47:24 +0200

    button style

--- main.js
+++ main.js
@@ -5,11 +5,12 @@ let buttonAdd = document.getElementById("buttonAdd");
 let DivInCorrectFormat = document.getElementById("inCorrectFormat");
 let btns = document.getElementsByTagName("button");
 for (const btn of btns) {
-    btn.addEventListener("touchstart", () => {
-        btn.style.transform = "scale(0.8)";   // уменьшаем кнопку до 80%
+    btn.addEventListener("touchstart", () => {// настройки вида кнопок при нажатии прописанные в css срабатывают не во всех браузерах, поэтому добавляю здесь
+        btn.style.transform = "scale(0.8)";
+        btn.style.backgroundColor ="#82c6e8";
     });
     btn.addEventListener("touchend", () => {
-        btn.style.transform = "scale(1)";     // возвращаем обратно
+        btn.style.transform = "scale(1)";
     });
 }
 
